8c24cdcf815c485b82acf8522e0a512a671d5884
lavanyapadaga5 <104807917+lavanyapadaga5@users.noreply.github.com>
2024-04-26 16:50:43 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import StudentList from './StudentList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      students: ['Lavs', 'anuu', 'Dharuu', 'Alee', 'Bhanuu'] // Array of student names
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Student List</h1>
+        <StudentList students={this.state.students} />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App;
